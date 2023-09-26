1d6b9eefd260fc6795ad8c58505891510dfe5ded
Desislava Gancheva <desislava.gancheva@p3-group.com>
2023-09-26 17:56:23 +0300

    first two tasks

---
+++ c#/linq/LinqTasks.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+void printEvenNumbers(int[] numbers)
+{
+    var evenNumbers =
+        from evenNumber in numbers
+        where evenNumber != null && evenNumber % 2 == 0
+        select evenNumber;
+
+    foreach (int evenNumber in evenNumbers)
+    {
+        Console.WriteLine(evenNumber);
+    }
+}
+
+int[] numbers = {1, 2, 3, 4, 5, 6, 7};
+
+printEvenNumbers(numbers);
+
+void prinNumbersInGivenDiapason(int beg, int end, int[] numbers)
+{
+    var numbersInDiapason =
+        from numberInDiapason in numbers
+        where numberInDiapason != null && numberInDiapason > beg && numberInDiapason < end
+        select numberInDiapason;
+    
+    foreach (int numberInDiapason in numbersInDiapason)
+    {
+        Console.WriteLine(numberInDiapason);
+    }    
+}
+
+prinNumbersInGivenDiapason(3, 6, numbers);
